0bcb24c9c5f0129429085b4ef12e601fdfa95109
Othman Djuliarso <othmandjuliarso@gmail.com>
2023-02-02 21:44:14 -0800

    modified some files

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 # todo_api/settings.py
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@zb7yfwh)#(s$4h08nlfc+4r!kme)7y25@m@ni0+0+w(@d$@ix'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
